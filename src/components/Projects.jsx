89f482c76fa1260e7471dc0b8acd18d5ed5c4ac9
Sophia Ibarrola <sophia@ibarrola.net>
2025-10-25 11:11:44 -0400

    project tiles (with hyperlinks)

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,31 +8,36 @@ const Projects = () => {
             <h1 className={styles.header}>Projects</h1>
             
             <div className={styles.tileparent}> 
+
                 {/* Project 1 */}
-                <div className={styles.tile}>
-                    <h2 className={styles.header}>
-                        College Hockey Award Show
-                    </h2>
-                    <h4 className={styles.subheader}>
-                        DS 210 Project
-                    </h4>
-                    <p className={styles.bodytext}>
-                        Built a program that analyzed college hockey statistics and gave awards based on the NHL end-of-season trophies
-                    </p>
-                </div>
+                <a href="https://github.com/sibarrol/DS210-Project">
+                    <div className={styles.tile}>
+                        <h2 className={styles.header}>
+                            College Hockey Award Show
+                        </h2>
+                        <h4 className={styles.subheader}>
+                            DS 210 Project
+                        </h4>
+                        <p className={styles.bodytext}>
+                            Built a program that analyzed college hockey statistics and gave awards based on the NHL end-of-season trophies
+                        </p>
+                    </div>
+                </a>
 
                 {/* Project 2 */}
-                <div className={styles.tile}>
-                    <h2 className={styles.header}>
-                        Hackathon Winner
-                    </h2>
-                    <h4 className={styles.subheader}>
-                        BU Spark! Mini Hacks Hackathon 2024 
-                    </h4>
-                    <p className={styles.bodytext}>
-                        Won a hackathon with my team by creating an app for voter information awareness.
-                    </p>
-                </div>
+                <a href="https://www.bu.edu/spark/2024/10/31/bu-spark-hosts-welcome-back-mini-hack-empowering-students-to-tackle-social-challenges/">
+                    <div className={styles.tile}>
+                        <h2 className={styles.header}>
+                            Hackathon Winner
+                        </h2>
+                        <h4 className={styles.subheader}>
+                            BU Spark! Mini Hacks Hackathon 2024 
+                        </h4>
+                        <p className={styles.bodytext}>
+                            Won a hackathon with my team by creating an app (ElectoRally) for voter information awareness.
+                        </p>
+                    </div>
+                </a>
 
             </div>
 
